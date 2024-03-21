91fd717ce94e960fd5cef63ff00e3ff64c829fdf
MS Rahman <msrahman048gmail.com>
2024-03-21 22:35:25 +0600

    md-46-1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+  return (
+    <>
+      <h1>Simple Form Submit</h1>
+      <SimpleForm/>
+    </>
+  )
+}
+
+export default App
