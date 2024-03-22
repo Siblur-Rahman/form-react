1ff12b16345545d33f72ccb5088b51515c1e4c35
MS Rahman <msrahman048gmail.com>
2024-03-22 15:18:40 +0600

    md-46-3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
-import StatefulForm from './StatefulForm/StatefulForm'
+// import StatefulForm from './StatefulForm/StatefulForm'
+
+import RefForm from "./components/RefForm/RefForm"
 
 function App() {
   return (
     <>
-      <h1>Simple Form Submit</h1>
-      <StatefulForm/>
+      {/* <h1>Simple Form Submit</h1>
+      <StatefulForm/> */}
+      <RefForm/>
     </>
   )
 }
