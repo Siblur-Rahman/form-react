c95d4a475750e9e07f42c16ba1549dfec849229e
MS Rahman <msrahman048gmail.com>
2024-03-23 12:00:36 +0600

    md-46-6 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 
 import './App.css'
-import ReusableForm from './components/ReusableForm/ReusableForm'
+import Grandpa from './components/Grandpa/Grandpa'
+// import ReusableForm from './components/ReusableForm/ReusableForm'
 // import HookForm from './components/HookForm/HookForm'
 // import StatefulForm from './StatefulForm/StatefulForm'
 // import RefForm from "./components/RefForm/RefForm"
 
 
 function App() {
-    const handleSingUpSubmit = data =>{
-      console.log('sign up data', data)
-  }
-  const handleUpdateProfile = data =>{
-    console.log('update profile', data);
-  }
+//     const handleSingUpSubmit = data =>{
+//       console.log('sign up data', data)
+//   }
+//   const handleUpdateProfile = data =>{
+//     console.log('update profile', data);
+//   }
   return (
     <>
+    <h1>Form Master</h1>
+    <Grandpa></Grandpa>
       {/* <h1>Simple Form Submit</h1>
       <StatefulForm/> */}
       {/* <RefForm/> */}
       {/* <HookForm/> */}
-      <ReusableForm 
+      {/* <ReusableForm 
       formTitle={'Sign Up'} 
       handleSubmit={handleSingUpSubmit}>
       <div>
@@ -30,7 +33,9 @@ function App() {
       <ReusableForm formTitle={'Profile Update'} handleSubmit={handleUpdateProfile}>
       <h2>Update profile</h2>
       <p>Always keep your profile update</p>
-      </ReusableForm>
+      </ReusableForm> */}
+
+      6:07
     </>
   )
 }
